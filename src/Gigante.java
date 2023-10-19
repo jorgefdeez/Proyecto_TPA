2dadc57104b9f47f486a2b76acd68a8fa7d8ca08
Jorge Fernández García <145361133+jorgefdeez@users.noreply.github.com>
2023-10-19 11:05:16 +0200

    Gigante

--- src/Gigante.java
+++ src/Gigante.java
@@ -1,21 +1,67 @@
 import java.util.Random;
 import java.util.Scanner;
-public class Gigante extends Enemigo{
+
+public class Gigante{
     private boolean se_ha_eliminado;
-    public void setSe_ha_eliminado(boolean _se_ha_eliminado){
-        this.se_ha_eliminado = _se_ha_eliminado;
+    private boolean curacion_disponible;
+    private int ataque;
+    private int Curacion;
+    private int vida;
+    private int defensa;
+    private int vidaTotal;
+    private Random random = new Random();
+
+    public Gigante(){
+        Scanner scanner = new Scanner(System.in);
+
+        ataque = random.nextInt(10)+7;
+        Curacion = 30;  //habilidad del gigante, se cura.
+        vida = 100;
+        defensa = 50;   //la defensa se complementa a la vida.
+        vidaTotal = vida + defensa;
+        se_ha_eliminado =  false;
+        curacion_disponible = true;
     }
-    public boolean getSe_ha_eliminado_jefe() {
-        return super.getSe_ha_eliminado_jefe();
+    public int getVida() {
+        return vida;
     }
-    public void setAtaque() {
-        Random random = new Random();
-        int a = random.nextInt(10)+7;
-        super.setAtaque(a);
+    public int getAtaque() {
+        return ataque = random.nextInt(10)+7;
     }
-    public void setVida() {
-        int v = 150;
-        super.setVida(v);
+    public int getDefensa() {
+        return defensa;
     }
 
+    public int getVidaTotal() {
+        return vidaTotal;
+    }
+
+    public int getCuracion() {
+        return Curacion;
+    }
+    public boolean getSe_ha_eliminado() {
+        return se_ha_eliminado;
+    }
+    public boolean getCuracion_disponible() {
+        return curacion_disponible;
+    }
+
+    public void setAtaque(int ataque) {
+        this.ataque = ataque;
+    }
+    public void setDefensa(int defensa) {
+        this.defensa = defensa;
+    }
+    public void setVida(int vida) {
+        this.vida = vida;
+    }
+    public void setVidaTotal(int vidaTotal) {
+        this.vidaTotal = vidaTotal;
+    }
+    public void setSe_ha_eliminado(boolean S){
+        this.se_ha_eliminado = S;
+    }
+    public void setCuracion_disponible(boolean s){
+        this.curacion_disponible = s;
+    }
 }
