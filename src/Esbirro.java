23935dd82183f8399a25adabc26b5784abe3172e
Jorge Fernández García <145361133+jorgefdeez@users.noreply.github.com>
2023-10-19 11:05:07 +0200

    Esbirro

--- src/Esbirro.java
+++ src/Esbirro.java
@@ -1,24 +1,56 @@
 import java.util.Random;
 import java.util.Scanner;
-public class Esbirro extends Enemigo{
+
+public class Esbirro{
     private boolean se_ha_eliminado;
-    public void setSe_ha_eliminado(boolean _se_ha_eliminado){
-        this.se_ha_eliminado = _se_ha_eliminado;
+    private int ataque;
+    private int vida;
+    private int defensa;
+    private int vidaTotal;
+    private Random random = new Random();
+    private  Scanner scanner = new Scanner(System.in);
+
+    public Esbirro(){
+
+        ataque = random.nextInt(10)+15;
+        vida = 100;
+        defensa = 10;   //la defensa se complementa a la vida.
+        vidaTotal = vida + defensa;
+        se_ha_eliminado = false;
     }
-    public boolean getSe_ha_eliminado_jefe() {
-        return super.getSe_ha_eliminado_jefe();
-    }
-    public void ataqueBasico() {
-        Random random = new Random();
-        int a = random.nextInt(12)+10;
-        super.setAtaque(a);
+    public int getVida() {
+        return vida;
     }
     public int getAtaque() {
-        return super.getAtaqueBasico();
+        return ataque = random.nextInt(10)+15;
     }
-    public void setVida() {
-        int v = 100;
-        super.setVida(v);
+    public int getDefensa() {
+        return defensa;
     }
 
+    public int getVidaTotal() {
+        return vidaTotal;
+    }
+
+    public boolean getSe_ha_eliminado() {
+        return se_ha_eliminado;
+    }
+
+    public void setAtaque(int ataque) {
+        this.ataque = ataque;
+    }
+    public void setDefensa(int defensa) {
+        this.defensa = defensa;
+    }
+    public void setVida(int vida) {
+        this.vida = vida;
+    }
+    public void setVidaTotal(int vidaTotal) {
+        this.vidaTotal = vidaTotal;
+    }
+    public void setSe_ha_eliminado(boolean S){
+        this.se_ha_eliminado = S;
+    }
 }
+
+
