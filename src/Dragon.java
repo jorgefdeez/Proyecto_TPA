4a268dd1f057657dd3cdb35dc517174aff2288e6
Jorge Fernández García <145361133+jorgefdeez@users.noreply.github.com>
2023-10-19 11:04:45 +0200

    Dragon

--- src/Dragon.java
+++ src/Dragon.java
@@ -1,20 +1,65 @@
 import java.util.Random;
 import java.util.Scanner;
-public class Dragon extends Enemigo{
+
+public class Dragon{
     private boolean se_ha_eliminado;
-    public void setSe_ha_eliminado(boolean _se_ha_eliminado){
-        this.se_ha_eliminado = _se_ha_eliminado;
+    private int ataque;
+    private int Escupe_Fuego;
+    private int vida;
+    private int defensa;
+    private int vidaTotal;
+    private boolean Escupe_Fuego_Disponible;
+    private Random random = new Random();
+    public Dragon(){
+        Scanner scanner = new Scanner(System.in);
+
+        ataque = random.nextInt(10)+20;
+        Escupe_Fuego = random.nextInt(15)+25;
+        vida = 120;
+        defensa = 0;   //la defensa se complementa a la vida.
+        vidaTotal = vida + defensa;
+        Escupe_Fuego_Disponible = true;
     }
-    public boolean getSe_ha_eliminado_jefe() {
-        return super.getSe_ha_eliminado_jefe();
+    public int getVida() {
+        return vida;
     }
-    public void SetataqueBasico() {
-        Random random = new Random();
-        int a = random.nextInt(15)+10;
+    public int getAtaque() {
+        return ataque = random.nextInt(10)+15;
     }
-    public void setVida() {
-        int v = 120;
-        super.setVida(v);
+    public int getDefensa() {
+        return defensa;
     }
 
+    public int getVidaTotal() {
+        return vidaTotal;
+    }
+
+    public int getEscupe_Fuego() {
+        return Escupe_Fuego;
+    }
+    public boolean getSe_ha_eliminado() {
+        return se_ha_eliminado;
+    }
+    public boolean getEscupe_Fuego_Disponible() {
+        return Escupe_Fuego_Disponible;
+    }
+
+    public void setAtaque(int ataque) {
+        this.ataque = ataque;
+    }
+    public void setDefensa(int defensa) {
+        this.defensa = defensa;
+    }
+    public void setVida(int vida) {
+        this.vida = vida;
+    }
+    public void setVidaTotal(int vidaTotal) {
+        this.vidaTotal = vidaTotal;
+    }
+    public void setSe_ha_eliminado(boolean S){
+        this.se_ha_eliminado = S;
+    }
+    public void setEscupe_Fuego_Disponible(boolean s){
+        Escupe_Fuego_Disponible = s;
+    }
 }
