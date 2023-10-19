0e5658f216bc72d05fc2d4e3419be4e48143efad
Jorge Fernández García <145361133+jorgefdeez@users.noreply.github.com>
2023-10-19 14:19:16 +0200

    main

--- src/main.java
+++ src/main.java
@@ -5,50 +5,38 @@ public class main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String nombre;
+        int op;
         int ID;
-        Jugador J1 = new Jugador();
-        System.out.print("Intro el nombre: ");
-        nombre = scanner.next();
-        J1.setNombre(nombre);
-        System.out.print("Introdcue el ID(4 numeros enteros): ");
-        ID = scanner.nextInt();
-        J1.setID(ID);
-        System.out.println("Bienvenido al juego " + J1.getNombre() + "!");
-        MecanicaJuego juego = new MecanicaJuego();
-        juego.jugar();
-        /*
-        System.out.println("Que heroe quieres usar? (Valquiria, Barbaro, Mago)");
-        int eleccion = scanner.nextInt();
-        switch (eleccion){
-            case 1:
-            {
-                Valquiria V1;
-                System.out.print("Bienvenido al juego " + J1.getNombre() + "!");
-                MecanicaJuego juego = new MecanicaJuego();
-                juego.jugar();
-            }
-            case 2:
-            {
-                Barbaro B1;
-                System.out.print("Bienvenido al juego " + J1.getNombre() + "!");
-                MecanicaJuego juego = new MecanicaJuego();
-                juego.jugar();
-            }
-            case 3:
-            {
-                Mago M1;
-                System.out.print("Bienvenido al juego " + J1.getNombre() + "!");
-                MecanicaJuego juego = new MecanicaJuego();
-                juego.jugar();
-            }
-            default:
-                System.out.println("Opcion invalida...");
-                break;
+        ArrayList<Jugador> lista_jugadores = new ArrayList<Jugador>();
+
+        System.out.print("Cuantos jugadores van a ser?: ");
+        op = scanner.nextInt();
+
+        for(int i=0;i<op;i++){
+            Jugador J1 = new Jugador();
+
+            System.out.print("Intro el nombre: ");
+            nombre = scanner.next();
+            J1.setNombre(nombre);
+            System.out.print("Introdcue el ID(4 numeros enteros): ");
+            ID = scanner.nextInt();
+            J1.setID(ID);
+            System.out.println("Bienvenido al juego " + J1.getNombre() + "!");
+            MecanicaJuego juego = new MecanicaJuego();
+            juego.jugar();
+
+            String resultado = juego.hasta_donde_llego_jugador();
+            J1.setResultado(resultado);
+            lista_jugadores.add(J1);
+
         }
-        */
 
-
-
+        System.out.println("*******REGISTRO DE PARTIDA*******");
+        for(int i = 0;i<lista_jugadores.size();i++){
+            System.out.println("\nNombre: " + lista_jugadores.get(i).getNombre());
+            System.out.println("ID: " + lista_jugadores.get(i).getID());
+            System.out.println("Resultado: " + lista_jugadores.get(i).getResultado());
+        }
 
     }
 }
