97d882a46cbda778aae99b33287bc449a12ba189
Jorge Fernández García <145361133+jorgefdeez@users.noreply.github.com>
2023-10-23 16:21:34 +0200

    Jugador

--- src/Jugador.java
+++ src/Jugador.java
@@ -5,6 +5,7 @@ public class Jugador {
     private int ID;
     private int nivelXP;
     private String resultado;
+    private String eleccion_heroe;
     private int vida;
     private int ataque;
     private boolean esta_eliminado;
@@ -18,13 +19,12 @@ public class Jugador {
     public int getNivelXP(){
         return nivelXP;
     }
-
     public boolean getEsta_eliminado() {
         return esta_eliminado;
     }
-    public String getResultado(){
-        return resultado;
-    }
+    public String getResultado(){return resultado;}
+    public String getSeleccion_heroe(){return eleccion_heroe;}
+
     void setNombre(String n){
         this.nombre = n;
     }
@@ -39,6 +39,26 @@ public class Jugador {
     }
     void setResultado(String r){
         this.resultado = r;
+    }
+
+    public void Seleccionar_heroe(){
+        int eleccion;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Barbaro = 1");
+        System.out.println("Mago = 2");
+        System.out.println("Valquiria = 3");
+        System.out.print("Que heroe quieres utilizar?: ");
+        eleccion = scanner.nextInt();
+        switch (eleccion){
+            case 1:
+                eleccion_heroe = "Barbaro";
+                break;
+            case 2:
+                eleccion_heroe = "Mago";
+                break;
+            case 3:
+                eleccion_heroe = "Valquiria";
+        }
     }
 
 }
