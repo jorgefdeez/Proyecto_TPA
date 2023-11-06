35bf3d83f53467e49c794f0d4485359b9255b018
Jorge Fernández García <145361133+jorgefdeez@users.noreply.github.com>
2023-11-06 15:39:41 +0100

    Dragon

--- src/Dragon.java
+++ src/Dragon.java
@@ -3,35 +3,22 @@ import java.util.Scanner;
 
 public class Dragon extends Enemigo{
     private boolean se_ha_eliminado;
-    private int ataque;
     private int Escupe_Fuego;
-    private int vida;
-    private int defensa;
-    private int vidaTotal;
     private boolean Escupe_Fuego_Disponible;
     private Random random = new Random();
     public Dragon(){
         Scanner scanner = new Scanner(System.in);
 
-        ataque = random.nextInt(10)+20;
+        setAtaque(random.nextInt(10)+20);
         Escupe_Fuego = random.nextInt(15)+25;
-        vida = 120;
-        defensa = 0;   //la defensa se complementa a la vida.
-        vidaTotal = vida + defensa;
+        setVida(120);
+        setDefensa(0); //la defensa se complementa a la vida.;
+        setVida_total(getVida() + getDefensa());
+
         Escupe_Fuego_Disponible = true;
     }
-    public int getVida() {
-        return vida;
-    }
-    public int getAtaque() {
-        return ataque = random.nextInt(10)+15;
-    }
-    public int getDefensa() {
-        return defensa;
-    }
-    public int getVidaTotal() {
-        return vidaTotal;
-    }
+
+
     public int getEscupe_Fuego() {
         return Escupe_Fuego;
     }
@@ -40,19 +27,6 @@ public class Dragon extends Enemigo{
     }
     public boolean getEscupe_Fuego_Disponible() {
         return Escupe_Fuego_Disponible;
-    }
-
-    public void setAtaque(int ataque) {
-        this.ataque = ataque;
-    }
-    public void setDefensa(int defensa) {
-        this.defensa = defensa;
-    }
-    public void setVida(int vida) {
-        this.vida = vida;
-    }
-    public void setVidaTotal(int vidaTotal) {
-        this.vidaTotal = vidaTotal;
     }
     public void setSe_ha_eliminado(boolean S){
         this.se_ha_eliminado = S;
