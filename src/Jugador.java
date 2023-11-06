38a7dc7a7d00c1de20d913a652080faff2760865
Jorge Fernández García <145361133+jorgefdeez@users.noreply.github.com>
2023-11-06 15:41:22 +0100

    Jugador

--- src/Jugador.java
+++ src/Jugador.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Jugador {
+public abstract class Jugador {
     private String nombre;
     private int ID;
     private int nivelXP;
@@ -10,38 +10,51 @@ public class Jugador {
     private int ataque;
     private boolean esta_eliminado;
 
-    public String getNombre(){
+    public String getNombre() {
         return nombre;
     }
-    public int getID(){
+
+    public int getID() {
         return ID;
     }
-    public int getNivelXP(){
+
+    public int getNivelXP() {
         return nivelXP;
     }
+
     public boolean getEsta_eliminado() {
         return esta_eliminado;
     }
-    public String getResultado(){return resultado;}
-    public String getSeleccion_heroe(){return eleccion_heroe;}
 
-    void setNombre(String n){
+    public String getResultado() {
+        return resultado;
+    }
+
+    public String getSeleccion_heroe() {
+        return eleccion_heroe;
+    }
+
+    void setNombre(String n) {
         this.nombre = n;
     }
-    void setID(int i){
+
+    void setID(int i) {
         ID = i;
     }
-    void setNivelXP(int xp){
+
+    void setNivelXP(int xp) {
         this.nivelXP = xp;
     }
-    void setEsta_eliminado(boolean e){
+
+    void setEsta_eliminado(boolean e) {
         this.esta_eliminado = e;
     }
-    void setResultado(String r){
+
+    void setResultado(String r) {
         this.resultado = r;
     }
 
-    public void Seleccionar_heroe(){
+    public void Seleccionar_heroe() {
         int eleccion;
         Scanner scanner = new Scanner(System.in);
         System.out.println("Barbaro = 1");
@@ -49,7 +62,7 @@ public class Jugador {
         System.out.println("Valquiria = 3");
         System.out.print("Que heroe quieres utilizar?: ");
         eleccion = scanner.nextInt();
-        switch (eleccion){
+        switch (eleccion) {
             case 1:
                 eleccion_heroe = "Barbaro";
                 break;
@@ -60,5 +73,6 @@ public class Jugador {
                 eleccion_heroe = "Valquiria";
         }
     }
+    public abstract void ataque_normal(int vida_enemigo);
 
 }
